e4a199ad0c4a6166bc31c20d954c73bffd5e6187
Brennan <brennan.pease@gmail.com>
2022-11-21 08:22:21 -0600

    Added everything...Might go back and make disconnect button a toggle between connect/disconnect

--- src/YoChild.java
+++ src/YoChild.java
@@ -1,6 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.Objects;
 
 public class YoChild extends YoClient {
     private int yoCount;
@@ -13,44 +17,57 @@ public class YoChild extends YoClient {
     private final Label recvyoLabel = new Label("Received Yo Count: 0");
 
     private JButton howdyButton;
+    private JTextArea recvPane;
+    private JScrollPane scrollPane;
+    private JButton yoButton;
+    private JButton connectionButton;
 
     public YoChild(String host, int port) throws IOException {
         initUI();
-        if(connect()){
+
+        howdyButton = new JButton("Send Howdy");
+        howdyButton.addActionListener(e -> send(SEND_HOWDY));
+        bPanel.add(howdyButton);
+
+        connectionButton = new JButton("Disconnect");
+        connectionButton.addActionListener(e -> disconnect());
+        bPanel.add(connectionButton);
+        if(connect()) {
             listen();
         }
-    }
-
-
-    public void howdyButton(){
-        howdyButton = new JButton("Howdy");
 
     };
+
+
+
+
+
+
     @Override
     public void updateStats(char recvCh) throws NullPointerException {
         switch(recvCh){
             case SEND_YO:
                 yoCount++;
                 if (this.yoCount % 5 == 0) {
-                    updateConsoleText(yoLabel.getText());
+                    updateConsoleText(yoCount + " Yos sent.");
                 }
                 break;
             case RECV_YO:
                 recvYo++;
                 if (this.recvYo % 5 == 0) {
-                    updateConsoleText(recvyoLabel.getText());
+                    updateConsoleText(recvYo + " Yos received.");
                 }
                 break;
             case SEND_HOWDY:
                 howdyCount++;
                 if (this.howdyCount % 5 == 0) {
-                    updateConsoleText(howdyLabel.getText());
+                    updateConsoleText(howdyCount + " Howdys sent.");
                 }
                 break;
             case RECV_HOWDY:
                 recvHowdy++;
                 if (this.recvHowdy % 5 == 0) {
-                    updateConsoleText(recvhowdyLabel.getText());
+                    updateConsoleText(recvHowdy + " Howdys received.");
                 }
                 break;
             default:
